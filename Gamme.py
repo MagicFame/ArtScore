46f4f922a4aa25a81058012ce8358eeecfcd5df7
MagicFame <jeremie.uzan@gmail.com>
2018-05-13 14:46:21 +0200

    Update Gamme.py

--- Gamme.py
+++ Gamme.py
@@ -1,6 +1,13 @@
 class Gamme:
-    def __init__(self, nom):
+    nom = ""
+    notes = []
+    def __init__(self, nom, note):
         self.nom = nom
-        notes = []
+        self.notes = list(note)
+
+    def getNom(self):
+        return self.nom
+    def getNotes(self): #Retourne les notes de la gamme
+        return self.notes
 
 
